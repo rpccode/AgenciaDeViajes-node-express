7eb6c7ab872dd0141b5c7b2f5a08b16b608e06ae
Rudy Casilla <rpccode@gmail.com>
2022-02-06 14:15:42 -0500

    base de datos

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ const port = process.env.port || 4000;
 // habilitar pug_style
 app.set('view engine', 'pug');
 
+//obterner el year actual
+
+app.use((req, res,next)=>{
+
+    const year = new Date();        
+
+        res.locals.ActualYear = year.getFullYear();
+        res.locals.nombreSitio='Agencia de Viajes';
+    next();
+});
+
+
+
 //agrega el router
 app.use('/', router);
 
+//definir la carpeta publica
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- router/index.js
+++ router/index.js
@@ -3,9 +3,41 @@ import express from  "express";
 const router = express.Router();
 
 
-router.get('/', (req, res) => res.send('inicio'));
-router.get('/nosotros', (req, res) => res.send('Nosostros'));
-router.get('/contacto', (req, res) => res.send('contacto'));
+router.get('/', (req, res) => {
+    
+    
+    
+    res.render('Inicio',{ 
+        pagina:'Inicio'
+    })
+
+});
+
+router.get('/nosotros', (req, res) => {
+    
+    res.render('nosotros',{ 
+        pagina:'Nosotros'
+    })
+
+});
+router.get('/testimoniales', (req, res) => {
+    
+    
+    
+    res.render('Testimoniales',{ 
+        pagina:'Testimoniales'
+    })
+
+});
+router.get('/viajes', (req, res) => {
+    
+    
+    
+    res.render('Viajes',{ 
+        pagina:'Viajes'
+    })
+
+});
 
 
 export default router;
